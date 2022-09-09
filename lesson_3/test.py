a00e71910d5ed90368b1c92e3446fe1bc3a9c9d3
Eduard <Nollkol>
2022-09-09 22:49:38 +0500

    task_1

---
+++ lesson_3/test.py
@@ -0,0 +1,25 @@
+'''
+import random
+
+def gen():
+    a = int(input('n = '))
+    list = [random.randint(0, 10) for _ in range(a)]
+    return list
+
+result = gen()
+
+
+b = result[::-1]
+result_2 = []
+i = 0
+while i < len(result):
+    c = result[i] * b[i]
+    result_2.append(c)
+    result.pop()
+    b.pop()
+    i += 1
+print(result_2)
+
+n = int(input('n = '))
+print(gen(n))
+'''
